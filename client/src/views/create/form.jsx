80dcf3d3042894576dc8676b6f638df0eb7087f2
Matias Gigena <tute910@gmail.com>
2023-08-05 18:31:26 -0300

    form

--- client/src/views/create/form.jsx
+++ client/src/views/create/form.jsx
@@ -98,7 +98,7 @@ const CreateRecipe = () => {
 		<div className="relative px-4 py-10 bg-white rounded-3xl shadow-lg sm:rounded-3xl sm:p-20">
 			<div className="max-w-md mx-auto">
 				<div>
-					<h1 className="text-2xl font-semibold">Add a custom recipe!</h1>
+					<h1 className="text-2xl text-black font-semibold">Add a custom recipe!</h1>
 				</div>
 				<div className="divide-y divide-gray-200">
 					<form onSubmit={handleSubmit} className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
@@ -108,7 +108,7 @@ const CreateRecipe = () => {
             onChange={handleChange}
             name="name"
             onClick={handleInputChange}
-            onMouseLeave={handleMouseLeave2} autoComplete="off"  className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"  />
+            onMouseLeave={handleMouseLeave2} autoComplete="off"  className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 bg-white"  />
 							<label htmlFor="name" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Name your recipe</label>
               
 						</div>
@@ -120,7 +120,7 @@ const CreateRecipe = () => {
             onChange={handleChange}
             name="summary"
             onClick={handleInputChange}
-            onMouseLeave={handleMouseLeave3} autoComplete="off"   className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+            onMouseLeave={handleMouseLeave3} autoComplete="off"   className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 bg-white" placeholder="Password" />
 							<label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Describe your recipe</label>
 						</div>
 						<div className="relative">
@@ -129,7 +129,7 @@ const CreateRecipe = () => {
            onChange={handleChange}
            name="stepByStep"
            onClick={handleInputChange}
-           onMouseLeave={handleMouseLeave4} autoComplete="off"    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" />
+           onMouseLeave={handleMouseLeave4} autoComplete="off"    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 bg-white" />
 							<label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Preparation step by step</label>
 						</div>
 						<div className="relative">
@@ -138,7 +138,7 @@ const CreateRecipe = () => {
             onChange={handleChange}
             name="healthScore"
             onClick={handleInputChange}
-            onMouseLeave={handleMouseLeave5} autoComplete="off"   className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"  />
+            onMouseLeave={handleMouseLeave5} autoComplete="off"   className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 bg-white"  />
 							<label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Healthiness percentage</label>
 						</div>
             {errores.healthScore && (
@@ -150,7 +150,7 @@ const CreateRecipe = () => {
                    onChange={handleChange}
                    name="image"
                    onClick={handleInputChange}
-                   onMouseLeave={handleMouseLeave6} autoComplete="off"   className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"/>
+                   onMouseLeave={handleMouseLeave6} autoComplete="off"   className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 bg-white"/>
 							<label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Add an image by URL</label>
               </div>
               {errores.image && <p className={style.error}>{errores.image}</p>}
